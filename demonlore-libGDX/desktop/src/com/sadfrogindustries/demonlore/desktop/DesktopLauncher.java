3e6973f7db55ba6402c60ca23e63688b4113c99b
Pigmassacre <gulligaolle@gmail.com>
2015-02-13 16:16:07 +0100

    Added gdx-tools to build.gradle.

--- demonlore-libGDX/desktop/src/com/sadfrogindustries/demonlore/desktop/DesktopLauncher.java
+++ demonlore-libGDX/desktop/src/com/sadfrogindustries/demonlore/desktop/DesktopLauncher.java
@@ -2,11 +2,16 @@ package com.sadfrogindustries.demonlore.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.sadfrogindustries.demonlore.DemonLore;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		// Pack textures if any asset file has been modified. Can apparently be done like this:
+		// TexturePacker.processIfModified("input folder", "output folder", "name of output");
+
 		new LwjglApplication(new DemonLore(), config);
 	}
 }
