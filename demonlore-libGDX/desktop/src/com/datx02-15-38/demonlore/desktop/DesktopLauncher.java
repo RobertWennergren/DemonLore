565651e17e319262a26bb3580deead93e006d672
Pigmassacre <gulligaolle@gmail.com>
2015-02-13 15:01:49 +0100

    Added libGDX project.

---
+++ demonlore-libGDX/desktop/src/com/datx02-15-38/demonlore/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.datx02-15-38.demonlore.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.datx02-15-38.demonlore.DemonLore;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DemonLore(), config);
+	}
+}
